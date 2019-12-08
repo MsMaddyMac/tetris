6ded971bec61b358d7879b9608649defe12fff24
Maddy <mmcintosh2k14@gmail.com>
2019-12-08 05:40:56 -0500

    creates gameOver ternary in Tetris.js

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
@@ -13,16 +13,29 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+
+
+    console.log('re-render');
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={ createStage() } />
+                <Stage stage={stage} />
                 <aside>
-                    <div>
-                        <Display text="Score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    )}
                     <StartButton />
                 </aside>
             </StyledTetris>
